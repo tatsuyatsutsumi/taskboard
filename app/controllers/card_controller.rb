77043ba5c63cecd368e6a7e86c163be8f36de889
tatsuya.tsutusmi <tatsuya223@gmail.com>
2019-01-01 10:04:09 +0000

    card fin

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,10 +1,11 @@
 class CardController < ApplicationController
+  before_action :set_card, only: %i(show edit update destroy)
+
   def new
     @card = Card.new
     @list = List.find_by(id: params[:list_id])
   end
 
-  # ==========ここから編集する==========
   def create
     @card = Card.new(card_params)
     if @card.save
@@ -15,10 +16,34 @@ class CardController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+    # ここに追加する
+    @lists = List.where(user: current_user)
+  end
+
+  def update
+    if @card.update_attributes(card_params)
+      redirect_to :root
+    else
+      @card.valid?
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @card.destroy
+    redirect_to :root
+  end
+
   private
     def card_params
       params.require(:card).permit(:title, :memo, :list_id)
     end
-  # ==========ここまで編集する==========
 
+    def set_card
+      @card = Card.find_by(id: params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
-
-  # ==========ここから編集する==========
   resources :list, only: %i(new create edit update destroy) do
-    resources :card, only: %i(new create)
+    # この行を編集する
+    resources :card, except: %i(index)
   end
-  # ==========ここまで編集する==========
-
 end
