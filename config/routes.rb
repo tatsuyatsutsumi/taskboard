9cbfc37a47f35495006e048d2424e3b9af85954b
tatsuya.tsutusmi <tatsuya223@gmail.com>
2019-02-24 15:11:19 +0000

    add_background_image

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   resources :list, only: %i(new create edit update destroy) do
-    # この行を編集する
+    
     resources :card, except: %i(index)
   end
 end
